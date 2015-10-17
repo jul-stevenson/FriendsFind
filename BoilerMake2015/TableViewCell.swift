5d7988b5ff451cffadfad5ea35cc9ef075176f32
Julianna Stevenson <steve146@purdue.edu>
2015-10-17 19:38:18 -0400

    initial commit

---
+++ BoilerMake2015/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  BoilerMake2015
+//
+//  Created by Julianna Stevenson on 10/16/15.
+//  Copyright © 2015 BoilerMake2015. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
